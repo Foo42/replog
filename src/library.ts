6fecf52162a4a99d313e839fff6085b6376eda50
Julian Haeger <julian.haeger@evergreensmartpower.co.uk>
2018-06-29 17:33:17 +0100

    added a library file to export stuff in npm module

---
+++ src/library.ts
@@ -0,0 +1,2 @@
+export { ClockedLog, ClockedValue, ConflictResolutionFunc, dropConflicts, mergeLogs } from './clockedLog'
+export { Clock, compareClocks, currentTime, mergeClocks, incrementKey, ClockComparisonResult } from './vectorClock'
